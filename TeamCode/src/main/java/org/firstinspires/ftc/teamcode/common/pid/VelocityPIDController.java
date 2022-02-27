40084b14effeaa8087158b76e52351fb5cb523e6
sanjith_cher <sanjithc4@outlook.com>
2022-02-27 12:51:16 -0800

    Create VelocityPIDController.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/pid/VelocityPIDController.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.common.pid;
+
+public class VelocityPIDController {
+    double p;
+    double i;
+    double d;
+    double targetVelocity;
+    public VelocityPIDController(double velocity, double Kp,double Ki,double Kd) {
+        targetVelocity = velocity;
+        p = Kp;
+        i = Ki;
+        d = Kd;
+
+    }
+    public double controller(double currentVelocity) {
+        // P
+        double error = targetVelocity - currentVelocity;
+        
+        // I
+        i = 0;
+
+        // D
+        double d = 0;
+
+        return Math.tanh(p * error);
+    }
+}
